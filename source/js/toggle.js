3ee61351dabd83273ddd09f492dabde3028b6653
NadiaLev <57722339+NadiaLev@users.noreply.github.com>
2020-04-30 21:52:24 +0300

    Добавлен файл по заданию 10 toggle

---
+++ source/js/toggle.js
@@ -0,0 +1,15 @@
+var navMain = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".main-nav__toggle");
+var top = document.querySelector(".page-header__top");
+
+navMain.classList.remove("main-nav--nojs");
+
+navToggle.addEventListener("click", function () {
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
+  } else {
+    navMain.classList.add("main-nav--closed");
+    navMain.classList.remove("main-nav--opened");
+  }
+});
